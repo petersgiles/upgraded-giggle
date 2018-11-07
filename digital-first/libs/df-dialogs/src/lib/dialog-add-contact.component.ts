26bee590455fdb72078bb7bcf56e99d1a507fafc
Peter Giles <peter.s.giles@gmail.com>
2018-11-07 12:49:25 +1100

    contact dialog list filter

--- digital-first/libs/df-dialogs/src/lib/dialog-add-contact.component.ts
+++ digital-first/libs/df-dialogs/src/lib/dialog-add-contact.component.ts
@@ -11,11 +11,13 @@ export interface DialogContactsData {
   <mdc-dialog>
   <mdc-dialog-container>
     <mdc-dialog-surface>
-      <mdc-dialog-title>Select an account</mdc-dialog-title>
+      <mdc-dialog-title>Select a Contact</mdc-dialog-title>
       <mdc-dialog-content>
-        <mdc-list avatar>
-          <mdc-list-item *ngFor="let contact of contacts" [tabIndex]="0" (click)="closeDialog(contact)" >
-            <mdc-icon mdcListItemGraphic>person</mdc-icon>{{contact.name}}
+        <mdc-text-field label="Search" #contactFilterText fullwidth (input)="filterItem(contactFilterText.value)"></mdc-text-field>
+        <mdc-list twoLine avatar>
+          <mdc-list-item *ngFor="let contact of filteredItems" [tabIndex]="0" (click)="closeDialog(contact)" >
+            <mdc-icon mdcListItemGraphic>person</mdc-icon>
+            <mdc-list-item-text [secondaryText]="contact.email">{{contact.name}}</mdc-list-item-text>
           </mdc-list-item>
         </mdc-list>
       </mdc-dialog-content>
@@ -23,15 +25,35 @@ export interface DialogContactsData {
   </mdc-dialog-container>
 </mdc-dialog>
   `,
-  styles: []
+  styles: [`
+  .mdc-dialog__surface {
+    width: 800px;
+    max-height: 600px;
+    min-height: 600px;
+  }
+  `]
 })
 export class DialogAddContactComponent implements OnInit {
-  contacts: any
+  contacts: any[]
+  filteredItems: any[]
 
   constructor(public dialogRef: MdcDialogRef<DialogAddContactComponent>, @Inject(MDC_DIALOG_DATA) public data: DialogContactsData) { }
 
+  filterItem(value) {
+
+    if (!value) {
+      this.filteredItems = [...this.contacts]
+    } else {
+      this.filteredItems = [...this.contacts].filter(
+        item => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+     )
+    }
+
+ }
+
   ngOnInit() {
     this.contacts = this.data.contacts
+    this.filteredItems = [...this.contacts]
   }
 
   closeDialog(item): void {
