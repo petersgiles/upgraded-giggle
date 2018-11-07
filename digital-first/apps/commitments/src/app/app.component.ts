0992875fdb1c9358c2d1fb1d3d8ad961f9232b48
Timothy Pakkianathan <timothy.pakkianathan@pmc.gov.au>
2018-11-07 15:10:48 +1100

    Merged PR 4998: Adding logging component.
    Adding logging component.

--- digital-first/apps/commitments/src/app/app.component.ts
+++ digital-first/apps/commitments/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Router, NavigationEnd } from '@angular/router'
+import { LoggerService } from '@digital-first/df-logging'
 
 @Component({
   selector: 'digital-first-root',
@@ -8,9 +9,10 @@ import { Router, NavigationEnd } from '@angular/router'
 export class AppComponent implements OnInit, OnDestroy {
   title = 'policy-briefs'
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private logger: LoggerService) {}
 
   ngOnInit() {
+    this.logger.info('Logging from app component')
     this.router.events.subscribe(evt => {
       if (!(evt instanceof NavigationEnd)) {
         return
--- digital-first/libs/df-logging/src/index.ts
+++ digital-first/libs/df-logging/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/df-logging.module'
+export * from './lib/console-logger.service'
+export * from './lib/logger.service'
