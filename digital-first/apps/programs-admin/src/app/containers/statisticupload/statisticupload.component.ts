2b53ada1df78c06c93fe9d9303cfb2d9c765699f
david.mcgrath <david.mcgrath@pmc.gov.au>
2018-11-29 09:42:19 +1100

    .

--- digital-first/apps/programs-admin/src/app/containers/statisticupload/statisticupload.component.ts
+++ digital-first/apps/programs-admin/src/app/containers/statisticupload/statisticupload.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {AllStatistics, AllStatisticsGQL} from "../../generated/graphql";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {UUID} from "@digital-first/df-utils";
 import {map} from "rxjs/operators";
+import {PassthroughService} from "../../services/passthrough.service";
 
 @Component({
   selector: 'digital-first-statisticupload',
@@ -20,7 +20,7 @@ export class StatisticuploadComponent implements OnInit {
 
   statisticReports: AllStatistics.StatisticReports[];
 
-  constructor(private allStatistics: AllStatisticsGQL, private http: HttpClient) {
+  constructor(private allStatistics: AllStatisticsGQL, private http: HttpClient, private passthrough: PassthroughService) {
   }
 
   onSelect(statisticId) {
@@ -47,24 +47,13 @@ export class StatisticuploadComponent implements OnInit {
 
   onSubmit() {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'MessageType': 'UploadElectorateStatisticSpreadsheet',
-        'MessageNamespace': 'ProgramsManager.Messages',
-        'MessageId': UUID.UUID()
-      })
-    };
-
-    //set a unique name for the file as there is a unique constraint on the filename in the attachments table
-    const fileExtension = this.fileToUpload.name.substr(this.fileToUpload.name.lastIndexOf('.') + 1);
-    let fileName = `${UUID.UUID()}.${fileExtension}`;
-
     const formData = new FormData();
-    formData.append('file', this.fileToUpload, fileName);
 
-    formData.append('message', `{Notes: "${this.model.notes}", DataDate:"${this.model.dataDate}",FileName:"${fileName}", StatisticReportId:"${this.model.statisticReportId}"}`);
+    formData.append('file', this.fileToUpload, this.fileToUpload.name);
 
-    this.http.post('https://localhost:52629/api/sendmessage', formData, httpOptions).subscribe(value => console.log('TODO: handle error.'));
+    formData.append('message', `{Notes: "${this.model.notes}", DataDate:"${this.model.dataDate}",FileName:"${this.fileToUpload.name}", StatisticReportId:"${this.model.statisticReportId}"}`);
+
+    this.passthrough.sendMessageOnToBus(formData, 'UploadElectorateStatisticSpreadsheet').subscribe(value => console.log('TODO: handle error.'));
   }
 }
 
