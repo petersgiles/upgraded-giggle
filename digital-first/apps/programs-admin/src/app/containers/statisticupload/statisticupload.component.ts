53c2665519f6f43dd8b4db095e7bf420683fbe69
david.mcgrath <david.mcgrath@pmc.gov.au>
2018-12-05 10:59:35 +1100

    switch statistic to reactive form + reinstate auth refresh

--- digital-first/apps/programs-admin/src/app/containers/statisticupload/statisticupload.component.ts
+++ digital-first/apps/programs-admin/src/app/containers/statisticupload/statisticupload.component.ts
@@ -5,6 +5,8 @@ import {map} from "rxjs/operators";
 import {PassthroughService} from "../../services/passthrough.service";
 import {UploadElectorateStatisticSpreadsheet} from "@dsuite/programs-manager-messages";
 import {MdcSnackbar} from '@angular-mdc/web'
+import {FormBuilder} from '@angular/forms'
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'digital-first-statisticupload',
@@ -15,33 +17,45 @@ import {MdcSnackbar} from '@angular-mdc/web'
 export class StatisticuploadComponent implements OnInit, OnDestroy {
 
   fileToUpload: File;
-  model: StatisticUploadFormModel;
   statistics: AllStatistics.Statistics[];
   statisticReports: AllStatistics.StatisticReports[];
-  statisticsSub: Subscription;
+  statisticsSub$: Subscription;
+
+  statisticForm = this.formBuilder.group({
+    statisticId: [undefined, Validators.required],
+    statisticReportId: [undefined, Validators.required],
+    notes: [''],
+    dataDate: ['', Validators.required],
+    filename: [''],
+    file: [null, Validators.required],
+  });
 
   constructor(private allStatistics: AllStatisticsGQL,
               private passthrough: PassthroughService,
+              private formBuilder: FormBuilder,
               private snackbar: MdcSnackbar) {
   }
 
   get diagnostic() {
-    return JSON.stringify(this.model);
+    return `status: ${JSON.stringify(this.statisticForm.status)},  value: ${JSON.stringify(this.statisticForm.value)}`;
   }
 
   onSelectionChange(event: { index: any, value: any }) {
 
     if (event.index > -1) {
 
-      this.statisticReports = this.statistics.filter(value => value.id == event.value)[0].statisticReports;
+      if (event.index == 0) {
+        this.statisticReports = null;
+      }
+      else {
+        this.statisticReports = this.statistics.filter(value => value.id == event.value)[0].statisticReports;
+      }
+      this.statisticForm.patchValue({'statisticReportId': undefined});
     }
   }
 
   ngOnInit() {
-
-    this.model = new StatisticUploadFormModel();
-
-    this.statisticsSub = this.allStatistics.watch().valueChanges
+    this.statisticsSub$ = this.allStatistics.watch().valueChanges
       .pipe(map(result => result.data.statistics)).subscribe(value => {
           this.statistics = value.sort((a, b) => {
             if (a.name < b.name) {
@@ -54,7 +68,6 @@ export class StatisticuploadComponent implements OnInit, OnDestroy {
           });
         }
       );
-
   }
 
   fileChange(event) {
@@ -64,6 +77,23 @@ export class StatisticuploadComponent implements OnInit, OnDestroy {
     if (fileList.length == 1) {
 
       this.fileToUpload = fileList[0];
+
+      //TODO: revisit this to tidy it up
+      const reader = new FileReader();
+
+      if (event.target.files && event.target.files.length) {
+
+        reader.readAsDataURL(this.fileToUpload);
+
+        reader.onload = () => {
+
+          this.statisticForm.patchValue(
+            {filename: this.fileToUpload.name});
+          this.statisticForm.patchValue({
+            file: this.fileToUpload
+          });
+        };
+      }
     }
   }
 
@@ -72,15 +102,14 @@ export class StatisticuploadComponent implements OnInit, OnDestroy {
 
     const formData = new FormData();
 
-    formData.append('file', this.fileToUpload, this.fileToUpload.name);
-
     const message = new UploadElectorateStatisticSpreadsheet();
 
-    message.statisticReportId = this.model.statisticReportId;
-    message.dataDate = new Date(this.model.dataDate);
-    message.fileName = this.fileToUpload.name;
-    message.notes = this.model.notes;
+    message.statisticReportId = this.statisticForm.value['statisticReportId'];
+    message.fileName = this.statisticForm.value['filename'];
+    message.dataDate = new Date(this.statisticForm.value['dataDate']);
+    message.notes = this.statisticForm.value['notes'];
 
+    formData.append('file', this.statisticForm.value['file'], message.fileName);
     formData.append('message', JSON.stringify(message));
 
     this.passthrough
@@ -95,14 +124,6 @@ export class StatisticuploadComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
 
-    this.statisticsSub.unsubscribe();
+    this.statisticsSub$.unsubscribe();
   }
-}
-
-class StatisticUploadFormModel {
-  public id: number;
-  public dataDate: Date;
-  public notes: string;
-  public statisticReportId: string;
-  public statisticId: string;
 }
