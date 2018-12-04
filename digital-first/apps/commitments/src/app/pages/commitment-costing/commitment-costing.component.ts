4646786ef884673d9d5ba42726fd841ba7dd0583
Peter Giles <peter.s.giles@gmail.com>
2018-12-05 09:10:16 +1100

    costing page

---
+++ digital-first/apps/commitments/src/app/pages/commitment-costing/commitment-costing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'digital-first-commitment-costing',
+  templateUrl: './commitment-costing.component.html',
+  styleUrls: ['./commitment-costing.component.scss']
+})
+export class CommitmentCostingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- digital-first/apps/commitments/src/app/reducers/commitment-action/sharepoint/commitment-action-data.service.ts
+++ digital-first/apps/commitments/src/app/reducers/commitment-action/sharepoint/commitment-action-data.service.ts
@@ -4,15 +4,36 @@ import { CommitmentActionDataService } from '../commitment-action-data.service'
 import { Observable, of } from 'rxjs'
 import { DataResult, CommitmentActionsResult } from '../../../models'
 import { concatMap, map, tap } from 'rxjs/operators'
-import { byCommitmentIdQuery } from '../../../services/sharepoint/caml'
+import { byCommitmentIdQuery, byJoinTableQuery } from '../../../services/sharepoint/caml'
 import { mapCommitmentActions } from './maps'
 
 @Injectable({
     providedIn: 'root'
 })
 export class CommitmentActionDataSharePointService implements CommitmentActionDataService {
-    removeActionFromCommitment(payload: any): Observable<DataResult<{ commitment: number; }>> {
-        throw new Error('Method not implemented.')
+    removeActionFromCommitment(variables: any): Observable<DataResult<{ commitment: number; }>> {
+        const viewXml = byJoinTableQuery({ fieldA: { name: 'Commitment', id: variables.commitment }, fieldB: { name: 'ID', id: variables.action } })
+
+        return this.sharepoint.getItems({
+            listName: 'CommitmentAction',
+            viewXml: viewXml
+        }).pipe(
+            map(mapCommitmentActions),
+            map(result => result[0]),
+            concatMap(result =>
+                this.sharepoint.removeItem({
+                    listName: 'CommitmentAction', id: result.id
+                }).pipe(
+                    concatMap(_ =>
+                        of({
+                            loading: false,
+                            data: {
+                                commitment: result.commitment
+                            }
+                        }))
+                )
+            )
+        )
     }
     addActionToCommitment(payload: any): Observable<DataResult<{ commitment: number; }>> {
         throw new Error('Method not implemented.')
