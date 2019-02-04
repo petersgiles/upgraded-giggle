871866577600fe5d80207ce7a5a5407370e6093a
david.mcgrath <david.mcgrath@pmc.gov.au>
2019-02-04 13:00:58 +1100

    Remove error link

--- digital-first/apps/programs-admin/src/app/graphql.module.ts
+++ digital-first/apps/programs-admin/src/app/graphql.module.ts
@@ -1,42 +1,23 @@
-import {NgModule} from '@angular/core'
-import {HttpClient, HttpClientModule} from '@angular/common/http'
-import {ApolloModule, Apollo} from 'apollo-angular'
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http'
-import {InMemoryCache} from 'apollo-cache-inmemory'
-import {environment} from '../environments/environment'
-import {onError} from 'apollo-link-error'
+import { NgModule } from '@angular/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { ApolloModule, Apollo } from 'apollo-angular'
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { environment } from '../environments/environment'
 
 const uri = environment.datasource.dataServiceUrl
 
 @NgModule({
   exports: [HttpClientModule, ApolloModule, HttpLinkModule]
 })
-
 export class GraphQLModule {
   constructor(apollo: Apollo, private httpClient: HttpClient) {
-
-    const errorLink = onError(({graphQLErrors, networkError}) => {
-      if (graphQLErrors) {
-        graphQLErrors.map(({message, locations, path}) => {
-            // tslint:disable-next-line:no-console
-            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-            throw graphQLErrors
-          }
-        )
-      }
-      if (networkError) {
-        // tslint:disable-next-line:no-console
-        console.log(`[Network error]:`, networkError)
-        throw networkError
-      }
-    })
-
     const httpLink = new HttpLink(this.httpClient).create({
       uri: uri
     })
 
     apollo.create({
-      link: errorLink.concat(httpLink),
+      link: httpLink,
       cache: new InMemoryCache()
     })
   }
