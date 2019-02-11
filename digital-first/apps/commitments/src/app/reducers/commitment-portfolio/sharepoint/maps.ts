1eb6b31e2a3a560c39634ba01dba8bfaed62b0bf
Peter Giles <peter.s.giles@gmail.com>
2019-02-11 16:16:59 +1100

    SHAREPOINT DRAG DROP

--- digital-first/apps/commitments/src/app/reducers/commitment-portfolio/sharepoint/maps.ts
+++ digital-first/apps/commitments/src/app/reducers/commitment-portfolio/sharepoint/maps.ts
@@ -3,6 +3,7 @@ import { idFromLookup } from '@digital-first/df-sharepoint'
 export const mapCommitmentPortfolio = (item): any => ({
     id: item.ID,
     commitment: idFromLookup(item.Commitment),
-    contact: idFromLookup(item.Contact)
+    portfolio: idFromLookup(item.Portfolio),
+    Primary: item.Primary
 })
 export const mapCommitmentPortfolios = (items): any[] => items.map(mapCommitmentPortfolio)
