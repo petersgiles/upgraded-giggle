4d8190a2994fb813124649efb3c96d4adf4cc9bb
Peter Giles <peter.s.giles@gmail.com>
2019-02-11 11:09:00 +1100

    dollars and indefinite articles

--- digital-first/apps/commitments/src/app/components/commitment-mad-lib/commitment-mad-lib.component.ts
+++ digital-first/apps/commitments/src/app/components/commitment-mad-lib/commitment-mad-lib.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core'
-
+import * as indef from 'indefinite'
 @Component({
   selector: 'digital-first-commitment-mad-lib',
   templateUrl: './commitment-mad-lib.component.html',
@@ -12,4 +12,14 @@ export class CommitmentMadLibComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  public getIndefiniteArticle(term) {
+    return indef(term)
+  }
+
+  public getMBT(val: number) {
+    // val is entered in millions so X 1000
+    const m = val * 1000
+    return m
+  }
 }
