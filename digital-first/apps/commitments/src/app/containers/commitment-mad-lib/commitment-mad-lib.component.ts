74e5e7500038bebe5b25fd443f7d44ac5643aaa1
Peter Giles <peter.s.giles@gmail.com>
2019-02-13 08:16:34 +1100

    Madder Mad Libs

---
+++ digital-first/apps/commitments/src/app/containers/commitment-mad-lib/commitment-mad-lib.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core'
+import * as indef from 'indefinite'
+import { CommitmentPortfolioService } from '../../reducers/commitment-portfolio/commitment-portfolio.service'
+import { Commitment } from '../../reducers'
+import { Portfolio } from '../../models'
+import { Subscription, Observable } from 'rxjs'
+import { DeliveryLocationService } from '../../reducers/commitment-delivery-location/commitment-delivery-location.service'
+import { Electorate } from '../../models'
+import { MapPoint } from '@digital-first/df-map'
+
+@Component({
+  selector: 'digital-first-commitment-mad-lib',
+  templateUrl: './commitment-mad-lib.component.html',
+  styleUrls: ['./commitment-mad-lib.component.scss']
+})
+export class CommitmentMadLibComponent implements OnInit, OnDestroy {
+
+  _commitment: Commitment
+  commitmentPortfoliosSubscription$: Subscription
+  relatedPortfolios: Portfolio[]
+  mapPoint$: Observable<MapPoint[]>
+  electorate$: Observable<Electorate[]>
+
+  @Input()
+  set commitment(val: Commitment) {
+    this._commitment = val
+    if (val) {
+      this.cpsservice.getPortfoliosByCommitment(val.id)
+    }
+  }
+
+  get commitment() {
+    return this._commitment
+  }
+  constructor(private cpsservice: CommitmentPortfolioService, private dlsservice: DeliveryLocationService) {}
+
+  ngOnInit() {
+    this.commitmentPortfoliosSubscription$ = this.cpsservice.CommitmentPortfolios.subscribe(
+      next => {
+        // tslint:disable-next-line:no-console
+        console.log(next)
+        this.relatedPortfolios = next || []
+      }
+    )
+
+    this.mapPoint$ = this.dlsservice.MapPoints
+    this.electorate$ = this.dlsservice.Electorates
+  }
+
+  ngOnDestroy(): void {
+    this.commitmentPortfoliosSubscription$.unsubscribe()
+  }
+
+  public getIndefiniteArticle(term) {
+    if (term) {
+      return indef(term)
+    }
+    return term
+  }
+}
