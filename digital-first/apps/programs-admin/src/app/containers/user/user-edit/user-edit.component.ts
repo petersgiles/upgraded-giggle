70fe955cd297e67ae5aaf64323fcb520b74eb4c2
jason.kitson <jason.kitson@pmc.gov.au>
2019-02-14 16:08:30 +1100

    format

--- digital-first/apps/programs-admin/src/app/containers/user/user-edit/user-edit.component.ts
+++ digital-first/apps/programs-admin/src/app/containers/user/user-edit/user-edit.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { SelectAgenciesGQL, SelectAgencies, GetUserGQL, UpdateUserGQL } from '../../../generated/graphql'
-import {Subscription, Observable} from 'rxjs'
-import {map} from 'rxjs/operators'
+import {
+  SelectAgenciesGQL,
+  SelectAgencies,
+  GetUserGQL,
+  UpdateUserGQL
+} from '../../../generated/graphql'
+import { Subscription, Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { FormBuilder, Validators } from '@angular/forms'
 import { Router, ActivatedRoute } from '@angular/router'
 
@@ -11,13 +16,14 @@ import { Router, ActivatedRoute } from '@angular/router'
   styleUrls: ['./user-edit.component.scss']
 })
 export class UserEditComponent implements OnInit {
-
-  constructor(private selectAgenciesGQL: SelectAgenciesGQL,
+  constructor(
+    private selectAgenciesGQL: SelectAgenciesGQL,
     private formBuilder: FormBuilder,
     private getUserGQL: GetUserGQL,
     private updateUserGQL: UpdateUserGQL,
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute
+  ) {}
 
   agencies$: Observable<SelectAgencies.Agencies[]>
   userSubscription$: Subscription
@@ -30,33 +36,34 @@ export class UserEditComponent implements OnInit {
   ngOnInit() {
     this.userId = this.route.snapshot.paramMap.get('id')
     this.agencies$ = this.selectAgenciesGQL
-    .fetch({}, { fetchPolicy: 'network-only' })
-    .pipe(map(result => result.data.agencies))
+      .fetch({}, { fetchPolicy: 'network-only' })
+      .pipe(map(result => result.data.agencies))
 
     this.userSubscription$ = this.getUserGQL
-    .fetch({ id: this.userId}, { fetchPolicy: 'no-cache' })
-    .pipe(map(value => value.data.user))
-    .subscribe(user => {
-      this.editUserForm.patchValue(
-        {
+      .fetch({ id: this.userId }, { fetchPolicy: 'no-cache' })
+      .pipe(map(value => value.data.user))
+      .subscribe(user => {
+        this.editUserForm.patchValue({
           emailAddress: user.emailAddress,
           agencyId: user.agency.id
-        }
-      )
-      this.rowVersion = user.rowVersion
-    })
-
+        })
+        this.rowVersion = user.rowVersion
+      })
   }
   onSubmit() {
-    this.updateUserGQL.mutate({
-      data: {
-        emailAddress: this.editUserForm.value['emailAddress'],
-        agencyId: this.editUserForm.value['agencyId'],
-        rowVersion: this.rowVersion,
-        id: this.userId
-      }
-    }, {}).subscribe(({data}) =>
-      this.router.navigate(['users', this.userId]))
+    this.updateUserGQL
+      .mutate(
+        {
+          data: {
+            emailAddress: this.editUserForm.value['emailAddress'],
+            agencyId: this.editUserForm.value['agencyId'],
+            rowVersion: this.rowVersion,
+            id: this.userId
+          }
+        },
+        {}
+      )
+      .subscribe(({ data }) => this.router.navigate(['users', this.userId]))
   }
   cancel() {
     return this.router.navigate(['users', this.userId])
