12495bea28ad8a44e0bc9a468833052c4c83ff5f
anzhang <ningmengvv@gmail.com>
2019-03-21 16:09:31 +1100

    a few things for this bryntum scheudler
    a few things for this bryntum scheudler

--- digital-first/apps/commitments-reader/src/app/components/planner/planner.component.ts
+++ digital-first/apps/commitments-reader/src/app/components/planner/planner.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
+import {SchedulerComponent} from "../scheduler/scheduler.component";
 
 @Component({
   selector: 'digital-first-planner',
   templateUrl: './planner.component.html',
   styleUrls: ['./planner.component.scss']
 })
-export class PlannerComponent {}
+export class PlannerComponent {
+  @ViewChild(SchedulerComponent) scheduler: SchedulerComponent;
+
+  events = [
+    { id : 1, name : 'First event', startDate : new Date(2018, 8, 27), duration : 1 },
+  ];
+  
+  resources = [
+    { id : 1, name : 'First resource' },
+  ];
+  
+  startDate = new Date(2018, 1, 7, 8);
+  endDate = new Date(2018, 1, 7, 19);
+}
