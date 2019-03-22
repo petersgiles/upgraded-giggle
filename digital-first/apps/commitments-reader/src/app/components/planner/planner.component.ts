ce5c1c66d837db4a4cf968dd20c1a0b5912effa6
Valdis Thomann <valdis.thomann@gmail.com>
2019-03-22 14:50:23 +1100

    tweaking timeranges

--- digital-first/apps/commitments-reader/src/app/components/planner/planner.component.ts
+++ digital-first/apps/commitments-reader/src/app/components/planner/planner.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core'
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core'
 import { SchedulerComponent } from '../scheduler/scheduler.component'
 import { Moment } from 'moment'
 import moment = require('moment')
@@ -6,7 +6,8 @@ import moment = require('moment')
 @Component({
   selector: 'digital-first-planner',
   templateUrl: './planner.component.html',
-  styleUrls: ['./planner.component.scss']
+  styleUrls: ['./planner.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PlannerComponent {
   @ViewChild(SchedulerComponent) scheduler: SchedulerComponent
@@ -47,31 +48,25 @@ export class PlannerComponent {
   timeRanges = [
     {
       name: 'Both sitting',
-      startDate: '2019-04-02 00:00',
+      startDate: '2019-04-02',
       duration: 2,
       cls: 'timerange-sitting-both'
     },
     {
       name: 'House sitting',
-      startDate: '2019-04-04 00:00',
+      startDate: '2019-04-04',
       duration: 1,
       cls: 'timerange-sitting-house'
     },
     {
       name: 'House sitting',
-      startDate: '2019-04-15 00:00',
-      endDate: '2019-04-18 0:00',
+      startDate: '2019-04-15',
+      endDate: '2019-04-19',
       cls: 'timerange-sitting-house'
     },
     {
-      name: 'International travel',
-      startDate: '2019-04-19 00:00',
-      endDate: '2019-04-21 0:00',
-      cls: 'timerange-sitting-senate'
-    },
-    {
       name: 'State election',
-      startDate: '2019-04-22 00:00'
+      startDate: '2019-04-22'
     }
   ]
 }
