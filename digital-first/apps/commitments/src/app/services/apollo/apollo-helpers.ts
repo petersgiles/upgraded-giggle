701022282f4edea2a79e766b6dbd7a56b6742f95
Peter Giles <peter.s.giles@gmail.com>
2019-03-28 09:44:15 +1100

    DataResultCallback<any, DataResult<T>>

--- digital-first/apps/commitments/src/app/services/apollo/apollo-helpers.ts
+++ digital-first/apps/commitments/src/app/services/apollo/apollo-helpers.ts
@@ -1,14 +1,14 @@
-import { Apollo } from 'apollo-angular'
-import { Injectable } from '@angular/core'
-import { Observable, of, throwError } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import { DataResult } from '../../models'
 import { switchMap, catchError } from 'rxjs/operators'
+
+export type DataResultCallback<T, TResult> =  (item: T) => TResult
 
 export const callMutate = <T>(apollo, options: {
     mutation: any,
     fetchPolicy?: 'no-cache' | 'network-only',
     variables?: any
-}, mapper?: any): Observable<DataResult<T>> => apollo
+}, mapper?:  DataResultCallback<any, DataResult<T>>): Observable<DataResult<T>> => apollo
     .mutate({
         ...options
     })
@@ -21,8 +21,6 @@ export const callMutate = <T>(apollo, options: {
         }),
         catchError(replyError)
     )
-
-type DataResultCallback<T, TResult> =  (item: T) => TResult
 
 export const callQuery = <T>(apollo, options: {
     query: any,
