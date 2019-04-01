003cd95ea2cd6516f5b83454a54340f72aca093a
Peter Giles <peter.s.giles@gmail.com>
2019-04-01 16:20:38 +1100

    deck seeds

--- backend/deck-gql/db/seeds/01_initial.js
+++ backend/deck-gql/db/seeds/01_initial.js
@@ -1,13 +1,27 @@
 
 const TABLES = require("../table-names.js");
+const data = require("./source/deckitems.json")
+const bulkInsert = require("../bulkInsert.js").bulkInsert;
+
+var deckItems = data.reduce(function(acc, item){
+  acc.push({
+       'id': item.Id,
+       'title': item.Title,
+       'description': item.SupportingText,
+       'size': item.Size,
+       'type': item.CardType,
+       'sortorder': item.SortOrder,
+       'colour': item.Colour,
+       'media': item.Media,
+       'data': item.Data,
+       'created_at': item.Created_x0020_Date,
+       'parent': item.Parent
+  })
+  return acc
+}, [])
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex(TABLES.DECK_ITEM).del()
-    .then(function () {
-      // Inserts seed entries
-      return knex(TABLES.DECK_ITEM).insert([
-        {id: 1, colName: 'rowValue1'}
-      ]);
-    });
+  .then(_ => bulkInsert(knex, TABLES.DECK_ITEM, deckItems));
 };
