d992415645c50d0b29004e5bae96f48750eeb8c6
Peter Giles <peter.s.giles@gmail.com>
2019-04-09 13:05:01 +1000

    some data

--- digital-first/apps/policy-briefs/src/app/pages/brief/brief.component.ts
+++ digital-first/apps/policy-briefs/src/app/pages/brief/brief.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core'
-import { BehaviorSubject, Subscription } from 'rxjs'
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { BehaviorSubject, Subscription, Observable } from 'rxjs'
 import { FormBuilder } from '@angular/forms'
-import { DocumentStatus } from '@df/components'
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators'
-import { discussionTree } from './mock-data';
-
-export const statuslist: DocumentStatus[] = [
-  {
-    id: '1',
-    icon: 'people',
-    caption: 'In Draft',
-    colour: 'Pink',
-    order: 1
-  },
-  {
-    id: '2',
-    icon: 'how_to_reg',
-    caption: 'Ready',
-    colour: 'GhostWhite',
-    order: 2
-  },
-  {
-    id: '3',
-    icon: 'cancel_presentation',
-    caption: 'Cancelled',
-    colour: 'Crimson',
-    order: 3
-  }
-]
+import { DocumentStatus, NavigatorTreeNode } from '@df/components'
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators'
+import { discussionTree, statuslist, navigatorData } from './mock-data'
+import { toTree, sortBy } from '@df/utils'
 
 const defaultBrief = {
   status: '1'
@@ -38,54 +15,103 @@ const defaultBrief = {
   templateUrl: './brief.component.html',
   styleUrls: ['./brief.component.scss']
 })
-export class BriefComponent implements OnInit {
+export class BriefComponent implements OnInit, OnDestroy {
+  public nodes$: Observable<any>
+  public navData$: BehaviorSubject<NavigatorTreeNode[]> = new BehaviorSubject(
+    []
+  )
 
-  background$: BehaviorSubject<string> = new BehaviorSubject('#455a64')
+  public background$: BehaviorSubject<string> = new BehaviorSubject('#455a64')
   public documentStatusList$: BehaviorSubject<any>
 
-  public comments$: BehaviorSubject<Comment[]> = new BehaviorSubject(discussionTree)
+  public comments$: BehaviorSubject<Comment[]> = new BehaviorSubject(
+    discussionTree
+  )
   public activeComment$: BehaviorSubject<Comment> = new BehaviorSubject(null)
 
   public form = this.fb.group({
     status: [null]
   })
-  formValueChangeSubscription$: Subscription
+
+  public formValueChangeSubscription$: Subscription
+  public storyData: NavigatorTreeNode[]
+  public nodesSubscription$: Subscription
+  public tree: any
 
   // tslint:disable-next-line:no-empty
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.storyData = navigatorData
+  }
 
   ngOnInit() {
+    this.navData$.next(this.storyData)
+
+    this.nodes$ = this.navData$.pipe(
+      map(nd =>
+        toTree(nd.sort(sortBy('order')), {
+          id: 'id',
+          parentId: 'parent',
+          children: 'children',
+          level: 'level'
+        })
+      )
+    )
+
+    this.nodesSubscription$ = this.nodes$.subscribe(p => (this.tree = p))
+
     this.documentStatusList$ = new BehaviorSubject(statuslist)
 
     this.form.patchValue(defaultBrief)
 
     this.formValueChangeSubscription$ = this.form.valueChanges
-    .pipe(
-      debounceTime(3000),
-      distinctUntilChanged()
-    )
-    .subscribe(blurEvent => {
-      this.handleChange(blurEvent)
-      this.formValueChangeSubscription$.unsubscribe()
-    })
+      .pipe(
+        debounceTime(3000),
+        distinctUntilChanged()
+      )
+      .subscribe(blurEvent => {
+        this.handleChange(blurEvent)
+        this.formValueChangeSubscription$.unsubscribe()
+      })
+  }
+
+  public ngOnDestroy() {
+    this.nodesSubscription$.unsubscribe()
   }
 
   handleChange($event) {
     const editedBrief = this.mapFormToBrief(this.form.value)
 
-    // emit editedBrief
+  // tslint:disable-next-line:no-console
+  console.log('🐛 - handleChange', $event)
   }
 
   mapFormToBrief(brief): any {
-    const map: any = {
-      ...brief,
-
+    const editedBrief: any = {
+      ...brief
     }
-    return map
+    return editedBrief
   }
 
   handleSubmit($event) {
     // tslint:disable-next-line:no-console
-    console.log('handleSubmit', $event)
+    console.log('🐛 - handleSubmit', $event)
+  }
+
+  handleSelect($event) {
+    // tslint:disable-next-line:no-console
+    console.log(`🐋 -  Select`, $event)
+  }
+
+  public handleSelectNavigatorNode(node) {
+    const navData = this.storyData.map(n => ({ ...n, active: false }))
+    const found = { ...navData.find(n => n.id === node.id) }
+    found.active = true
+    const list = [...navData.filter(n => n.id !== node.id), found]
+    this.navData$.next(list)
+  }
+
+  handleEvent($event, action) {
+    // tslint:disable-next-line:no-console
+    console.log(`🦍 - ${action}`, $event)
   }
 }
--- digital-first/apps/policy-briefs/src/app/pages/brief/mock-data.ts
+++ digital-first/apps/policy-briefs/src/app/pages/brief/mock-data.ts
@@ -1,4 +1,141 @@
 import { toTree } from '@df/utils'
+import { DocumentStatus, NavigatorTreeNode } from '@df/components'
+
+export const navigatorData: NavigatorTreeNode[] = [
+    {
+      id: 'ddf2e782-c890-5df3-8169-d94c099e60c1',
+      parent: null,
+      caption: 'qui in est',
+      meta: 'last updated 1/1/2019',
+      colour: 'GoldenRod',
+      order: 17,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'd28dd1d7-7a03-53e2-8620-7cc9801fb091',
+      parent: 'ddf2e782-c890-5df3-8169-d94c099e60c1',
+      caption: 'fugit sint officia',
+      meta: '3 new',
+      colour: 'GoldenRod',
+      order: 65,
+      active: false,
+      expanded: false
+    },
+    {
+      id: '6e163c94-eca8-54bf-9857-95988c2742c6',
+      parent: 'ddf2e782-c890-5df3-8169-d94c099e60c1',
+      caption: 'exercitationem illum voluptas',
+      meta: '',
+      colour: 'GoldenRod',
+      order: 54,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'b47463dc-63b7-5feb-b006-8558b6e67a72',
+      parent: 'd28dd1d7-7a03-53e2-8620-7cc9801fb091',
+      caption: 'aut eveniet voluptatem',
+      meta: 'last updated 1/1/2019',
+      colour: 'GoldenRod',
+      order: 41,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'b99c94cc-4c87-51c2-ad8b-71966991116b',
+      parent: '6e163c94-eca8-54bf-9857-95988c2742c6',
+      caption: 'quisquam natus enim',
+      meta: 'last updated 1/1/2019',
+      colour: 'GoldenRod',
+      order: 4,
+      active: false,
+      expanded: false
+    },
+    {
+      id: '329ce8ed-5e8c-52aa-984a-e3615463d392',
+      parent: null,
+      caption: 'quo iure similique',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 43,
+      active: false,
+      expanded: true
+    },
+    {
+      id: '6b7563f1-8e30-5d96-8495-9d99c3cf3fab',
+      parent: 'fec1cadd-0fff-599c-b1a4-49b7349a4d57',
+      caption: 'eos omnis nesciunt',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 99,
+      active: true,
+      expanded: false
+    },
+    {
+      id: 'e6976877-f03d-5a39-a8d0-3baee5365476',
+      parent: '329ce8ed-5e8c-52aa-984a-e3615463d392',
+      caption: 'quibusdam excepturi pariatur',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 1,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'fec1cadd-0fff-599c-b1a4-49b7349a4d57',
+      parent: 'e6976877-f03d-5a39-a8d0-3baee5365476',
+      caption: 'quia sunt facilis',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 39,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'fec1cadd-0fff-599c-b1a4-49b7349a4d51',
+      parent: 'fec1cadd-0fff-599c-b1a4-49b7349a4d57',
+      caption: '4 deep',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 39,
+      active: false,
+      expanded: false
+    },
+    {
+      id: 'e9861dad-d68e-5aa6-9ee9-96eb46534a4a',
+      parent: 'fec1cadd-0fff-599c-b1a4-49b7349a4d57',
+      caption: 'ipsam facilis totam',
+      meta: 'last updated 1/1/2019',
+      colour: 'Crimson',
+      order: 10,
+      active: false,
+      expanded: false
+    }
+  ]
+export const statuslist: DocumentStatus[] = [
+    {
+      id: '1',
+      icon: 'people',
+      caption: 'In Draft',
+      colour: 'Pink',
+      order: 1
+    },
+    {
+      id: '2',
+      icon: 'how_to_reg',
+      caption: 'Ready',
+      colour: 'GhostWhite',
+      order: 2
+    },
+    {
+      id: '3',
+      icon: 'cancel_presentation',
+      caption: 'Cancelled',
+      colour: 'Crimson',
+      order: 3
+    }
+  ]
 
 export const demoAuthor: any = {
   username: 'demoAuthor',
