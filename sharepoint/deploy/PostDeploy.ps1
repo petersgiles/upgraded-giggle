871d8e0154b261f05e0b0899cad7ac63ff559352
Timothy Pakkianathan <timothy.pakkianathan@pmc.gov.au>
2019-04-26 13:23:26 +1000

    Octopus variable to push reference data.

--- sharepoint/deploy/PostDeploy.ps1
+++ sharepoint/deploy/PostDeploy.ps1
@@ -2,7 +2,7 @@ param(
     [string]$SiteUrls = $OctopusParameters["SiteUrls"],
     [string]$AppName = $OctopusParameters["AppName"],
     [switch]$jsOnly,
-    [switch]$loadReferenceData
+    [bool]$LoadReferenceData = $OctopusParameters["LoadReferenceData"]
     
 )
 
@@ -20,8 +20,7 @@ foreach ($deploySiteUrl in $deploySites) {
     & .\scripts\BulkUploadSharePointCSOM.ps1 -Folder "SitePages" -DocLibName "Site Pages" -binPath $binPath -SiteUrl $deploySiteUrl -jsOnly:$jsOnly.IsPresent
     & .\scripts\Deploy-Lists.ps1 -saveLocation "ListDefinitions/$AppName" -binPath $binPath -SiteUrl $deploySiteUrl
     
-    if ($loadReferenceData.IsPresent) {
+    if ($LoadReferenceData) {
         & .\scripts\Import-ReferenceData.ps1 -dataFolder "ListData/$AppName" -binPath $binPath -SiteUrl $deploySiteUrl
-
     }
 }
