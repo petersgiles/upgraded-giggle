27091d899f066a2c1898e0be3f6ed2b5e559d205
Timothy Pakkianathan <timothy.pakkianathan@pmc.gov.au>
2019-04-30 16:16:37 +1000

    Check for folder before pushing the lists.

--- sharepoint/deploy/scripts/Deploy-Lists.ps1
+++ sharepoint/deploy/scripts/Deploy-Lists.ps1
@@ -29,13 +29,17 @@ function Get-ListsToProcess($saveLocation) {
 Add-Type -Path "$binPath\Microsoft.SharePoint.Client.dll"
 Add-Type -Path "$binPath\Microsoft.SharePoint.Client.Runtime.dll"
 
+if(-not (Test-Path $saveLocation))
+{
+    return
+}
 Write-Host "Deploying list schemas to $siteUrl from $saveLocation"
 
 $context = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
 HandleMixedModeWebApplication $context $binPath
 
 $listsAll = Get-ListsToProcess $saveLocation
-if($listsAll -eq $null)
+if($null -eq $listsAll)
 {
     return
 }
