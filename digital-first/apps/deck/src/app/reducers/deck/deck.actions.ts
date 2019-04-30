dec8ce1bcc76178bad58c708bec63867f087a1ac
Peter Giles <peter.s.giles@gmail.com>
2019-04-30 12:53:15 +1000

    more actions

--- digital-first/apps/deck/src/app/reducers/deck/deck.actions.ts
+++ digital-first/apps/deck/src/app/reducers/deck/deck.actions.ts
@@ -6,7 +6,9 @@ export enum DeckActionTypes {
   GetDeckItemsFailure = '[Deck] Get DeckItems Failure',
   AddDeckItem = '[Deck] AddDeckItem',
   UpdateDeckItem = '[Deck] UpdateDeckItem',
-  RemoveDeckItem = '[Deck] RemoveDeckItem'
+  RemoveDeckItem = '[Deck] RemoveDeckItem',
+  SetActiveParent = '[Deck] SetActiveParent',
+  SetSelectedDeckItem = '[Deck] SetSelectedDeckItem'
 }
 
 export class LoadDeck implements Action {
@@ -16,22 +18,22 @@ export class LoadDeck implements Action {
 
 export class GetDeckItems implements Action {
   readonly type = DeckActionTypes.GetDeckItems
-  constructor(public payload?: { parent: string}) {}
+  constructor(public payload?: { parent: string }) {}
 }
 
 export class AddDeckItem implements Action {
   readonly type = DeckActionTypes.AddDeckItem
-  constructor(public payload?: any) {}
+  constructor(public payload: any) {}
 }
 
 export class UpdateDeckItem implements Action {
   readonly type = DeckActionTypes.UpdateDeckItem
-  constructor(public payload?: any) {}
+  constructor(public payload: any) {}
 }
 
 export class RemoveDeckItem implements Action {
   readonly type = DeckActionTypes.RemoveDeckItem
-  constructor(public payload?: any) {}
+  constructor(public payload: { id: string}) {}
 }
 
 export class GetDeckItemsFailure implements Action {
@@ -39,4 +41,22 @@ export class GetDeckItemsFailure implements Action {
   constructor(public payload: any) {}
 }
 
-export type DeckActions = LoadDeck | GetDeckItems | GetDeckItemsFailure | AddDeckItem | UpdateDeckItem | RemoveDeckItem
+export class SetActiveParent implements Action {
+  readonly type = DeckActionTypes.SetActiveParent
+  constructor(public payload: { id: string}) {}
+}
+
+export class SetSelectedDeckItem implements Action {
+  readonly type = DeckActionTypes.SetSelectedDeckItem
+  constructor(public payload: { id: string}) {}
+}
+
+export type DeckActions =
+    LoadDeck
+  | GetDeckItems
+  | GetDeckItemsFailure
+  | AddDeckItem
+  | UpdateDeckItem
+  | RemoveDeckItem
+  | SetActiveParent
+  | SetSelectedDeckItem
