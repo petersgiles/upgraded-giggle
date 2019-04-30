b3526e6796673141b78fb3ecd0d1ec1ab257c9e4
Kim McClymont <Kim.McClymont@pmc.gov.au>
2019-04-30 11:58:35 +1000

    Added router stuff

--- digital-first/apps/commitments-reader/src/app/reducers/router.actions.ts
+++ digital-first/apps/commitments-reader/src/app/reducers/router.actions.ts
@@ -1,9 +1,12 @@
 import {Action} from '@ngrx/store'
 import { NavigationExtras } from '@angular/router'
+import { OperatorFunction } from 'rxjs'
+import { filter } from 'rxjs/operators'
 
 export const GO = '[Router] Go'
 export const BACK = '[Router] Back'
 export const FORWARD = '[Router] Forward'
+export const CHANGE = '[Router] Route Change'
 
 export class Go implements Action {
   readonly type = GO
@@ -27,3 +30,24 @@ export class Forward implements Action {
 }
 
 export type Actions = Go | Back | Forward
+
+export class RouteChange implements Action {
+  readonly type = CHANGE
+  constructor(public payload: { params: any, path: string }) {}
+}
+
+export function ofRoute(route: string | string[]): OperatorFunction<Action, Action> {
+  return filter((action: Action) => {
+      const isRouteAction = action.type === CHANGE;
+      if (isRouteAction) {
+          const routeAction = action as RouteChange;
+          const routePath = routeAction.payload.path;
+          if (Array.isArray(route)) {
+              return route.includes(routePath);
+          } else {
+              return routePath === route;
+          }
+      }
+      return isRouteAction;
+  });
+}
--- digital-first/apps/commitments-reader/src/app/reducers/router.effects.ts
+++ digital-first/apps/commitments-reader/src/app/reducers/router.effects.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core'
-import { Router } from '@angular/router'
+import { Router, ActivationEnd } from '@angular/router'
 import { Location } from '@angular/common'
 
 import { Effect, Actions, ofType } from '@ngrx/effects'
 import * as RouterActions from './router.actions'
 
 import { tap, map, filter } from 'rxjs/operators'
+
 import { Store } from '@ngrx/store';
 
 @Injectable()
@@ -16,7 +17,7 @@ export class RouterEffects {
     private location: Location,
     private store: Store<any>
   ) {
-    //this.listenToRouter();
+    this.listenToRouter();
   }
 
   @Effect({ dispatch: false })
@@ -29,13 +30,24 @@ export class RouterEffects {
 
   @Effect({ dispatch: false })
   navigateBack$ = this.actions$
-    .pipe(ofType(RouterActions.BACK))
-    .pipe(tap(() => this.location.back()))
+    .pipe(ofType(RouterActions.BACK),
+    tap(() => this.location.back()))
 
   @Effect({ dispatch: false })
   navigateForward$ = this.actions$
-    .pipe(ofType(RouterActions.FORWARD))
-    .pipe(tap(() => this.location.forward()))
+    .pipe(ofType(RouterActions.FORWARD),
+    tap(() => this.location.forward()))
 
-
+    private listenToRouter() {
+      this.router.events.pipe(
+          filter(event => event instanceof ActivationEnd)
+      ).subscribe((event: ActivationEnd) =>
+          this.store.dispatch(new RouterActions.RouteChange({
+              params: { ...event.snapshot.params },
+              path: event.snapshot.routeConfig.path
+          }))
+      );
+  }
 }
+
+
