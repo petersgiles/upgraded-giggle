2546fdb7b220f906d274db9a483d01089f8be067
Peter Giles <peter.s.giles@gmail.com>
2019-05-09 09:58:37 +1000

    clean up

--- digital-first/apps/commitments-reader/src/app/services/app-data/app-data.service.factory.ts
+++ digital-first/apps/commitments-reader/src/app/services/app-data/app-data.service.factory.ts
@@ -1,11 +1,8 @@
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-
 import { SharepointJsomService } from '@df/sharepoint'
 import { SharePointAppDataService } from './sharepoint/sharepoint-app-data.service'
 import { DevelopAppDataService } from './develop/develop-app-data.service'
 import { SettingsService } from '../settings.service'
-import { AppDataService } from './app-data.service';
+import { AppDataService } from './app-data.service'
 
 const appDataServiceFactory = (
   settings: SettingsService,
