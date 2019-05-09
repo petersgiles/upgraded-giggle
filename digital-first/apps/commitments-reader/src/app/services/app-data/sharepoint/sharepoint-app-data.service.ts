3709a6cbce22836595397f5d2124eddea1e32aae
Peter Giles <peter.s.giles@gmail.com>
2019-05-09 11:19:58 +1000

    package updates

--- digital-first/apps/commitments-reader/src/app/services/app-data/sharepoint/sharepoint-app-data.service.ts
+++ digital-first/apps/commitments-reader/src/app/services/app-data/sharepoint/sharepoint-app-data.service.ts
@@ -1,11 +1,30 @@
 import { Observable, of } from 'rxjs'
 import { Injectable } from '@angular/core'
 import { AppDataService } from '../app-data.service'
-import { SharepointJsomService, SPAppUserProfile } from '@df/sharepoint'
+import { SharepointJsomService, SPAppUserProfile, fromLookup } from '@df/sharepoint'
+import { concatMap } from 'rxjs/operators';
+import { DataResult, GroupPermissionsResult } from '../../../models'
+
+export const mapGroupPermission = (item): any => ({
+  id: item.ID,
+  rights: item.Rights,
+  component: item.Component,
+  group: fromLookup(item.Group).title
+      })
+
+export const mapGroupPermissions = (items): any[] => items.map(mapGroupPermission)
 
 export class SharePointAppDataService implements AppDataService {
-  getCurrentUserOperations(roles: { groupPermissions: any }): Observable<any> {
-    return of(null)
+  getCurrentUserOperations(roles: any): Observable<DataResult<GroupPermissionsResult>> {
+    return this.sharepoint.getItems({ listName: 'GroupPermission' })
+      .pipe(
+        concatMap((result: any) =>
+          of({
+            data: { groupPermissions: mapGroupPermissions(result) },
+            loading: false,
+            error: null
+          }))
+      )
   }
   get UserOperation(): Observable<any> {
     return of(null)
