3bf5dd1f29c1d34fd4185f6bff7a6785c9649c47
Kim McClymont <Kim.McClymont@pmc.gov.au>
2019-05-10 09:35:28 +1000

    Changed name of assets commitments reader to correct name

--- digital-first/apps/commitments-reader/src/app/reducers/user/user.reducer.ts
+++ digital-first/apps/commitments-reader/src/app/reducers/user/user.reducer.ts
@@ -1,13 +1,14 @@
 import { UserActions, UserActionTypes } from './user.actions'
 import { AppActions, AppActionTypes } from '../app/app.actions'
+import { createFeatureSelector, createSelector } from '@ngrx/store'
 
-export interface State {
+export interface UserState {
   currentUser
   drawerOpen
   operations
 }
 
-export const initialState: State = {
+export const initialState: UserState = {
   currentUser: null,
   drawerOpen: false,
   operations: {}
@@ -16,7 +17,7 @@ export const initialState: State = {
 export function reducer(
   state = initialState,
   action: UserActions | AppActions
-): State {
+): UserState {
   switch (action.type) {
     case UserActionTypes.SetCurrentUser: {
       return {
@@ -63,6 +64,54 @@ export function reducer(
       return state
   }
 }
-export const getOperations = (state: State) => state.operations
-export const getCurrentUser = (state: State) => state.currentUser
-export const getDrawerOpen = (state: State) => state.drawerOpen
+export const getOperations = (state: UserState) => state.operations
+export const getCurrentUser = (state: UserState) => state.currentUser
+export const getDrawerOpen = (state: UserState) => state.drawerOpen
+
+export const userState = createFeatureSelector<UserState>('user')
+
+export const getUserCurrentUser = createSelector(
+  userState,
+  getCurrentUser
+)
+
+export const getCurrentUserOperations = createSelector(
+  userState,
+  getUserCurrentUser,
+  getOperations,
+  (user, operations) => {
+    if (!user || !operations || !user.roles) {
+      return {
+        ...OPERATION_DEFAULTS
+      }
+    }
+
+    const rights = Object.keys(OPERATION_DEFAULTS).reduce((acc: any, componentName: any) => {
+
+        const operationsRights = user.roles.reduce((rightsAcc: any, group: any) => {
+            const groupComponents = operations[group]
+            if (groupComponents && groupComponents[componentName]) {
+                rightsAcc.push(groupComponents[componentName])
+            }
+            return rightsAcc
+        }, [])
+
+        const orderedRights = operationsRights.sort((left: any, right: any) =>
+            OPERATION_RIGHTS_PRECEDENT.indexOf(left) > OPERATION_RIGHTS_PRECEDENT.indexOf(right) ? 1 : -1)
+        acc[componentName] = orderedRights[0]
+        return acc
+    }, {})
+
+    const finalRights = Object.keys(rights).reduce((a, i) => {
+        if (rights[i]) {
+            a[i] =  rights[i]
+        }
+        return a
+    }, {})
+
+    return {
+      ...OPERATION_DEFAULTS,
+      ...finalRights
+    }
+  }
+)
