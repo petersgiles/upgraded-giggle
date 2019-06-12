242db050b7521f91006a4693c772decdb5f87968
anzhang <ningmengvv@gmail.com>
2019-06-12 18:14:30 +1000

    Working with Tim on fixing the sharepoint deployment stuff

--- sharepoint/Compile-Page.ps1
+++ sharepoint/Compile-Page.ps1
@@ -23,5 +23,6 @@ function Generate-Links($content, $template, $regex, $token) {
 
 $pageContent = Generate-Links -content $pageContent -token "{{css}}" -regex '<link href="(.*?)" rel="stylesheet"' -template ('<link runat="server" rel="stylesheet" type="text/css" href="<% $SPUrl:~site/SiteAssets/apps/' + $applicationName + '/{0} %>" />')
 $pageContent = Generate-Links -content $pageContent -token "{{scripts}}" -regex '<script type="text/javascript" src="(.*?)"' -template ('<SharePoint:ScriptLink runat="server" name="~Site/SiteAssets/apps/' + $applicationName + '/{0}"/>')
+$pageContent = Generate-Links -content $pageContent -token "{{scripts}}" -regex '<script src="(.*?)"' -template ('<SharePoint:ScriptLink runat="server" name="~Site/SiteAssets/apps/' + $applicationName + '/{0}"/>')
 
 $pageContent > $pageOutputPath
