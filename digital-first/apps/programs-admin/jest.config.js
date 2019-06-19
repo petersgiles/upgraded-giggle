3c6ff8507c35808f993cea04a6c49267026284f9
david.mcgrath <david.mcgrath@pmc.gov.au>
2019-06-19 13:56:41 +1000

    chore(programs-admin):  Remove programs-admin code

--- digital-first/apps/programs-admin/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'programs-admin',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/programs-admin/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-}
