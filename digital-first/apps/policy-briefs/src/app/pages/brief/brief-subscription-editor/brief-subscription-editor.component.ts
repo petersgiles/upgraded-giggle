83f753e9671e8bda8ceae5f6df33889a57777cf4
Peter Giles <peter.s.giles@gmail.com>
2019-07-01 08:47:17 +1000

    mock data

--- digital-first/apps/policy-briefs/src/app/pages/brief/brief-subscription-editor/brief-subscription-editor.component.ts
+++ digital-first/apps/policy-briefs/src/app/pages/brief/brief-subscription-editor/brief-subscription-editor.component.ts
@@ -2,18 +2,14 @@ import { Component, OnInit } from '@angular/core'
 import * as fromRoot from '../../../reducers/index'
 import * as fromBrief from '../../../reducers/brief/brief.reducer'
 import { select, Store } from '@ngrx/store'
-import {
-  switchMap,
-} from 'rxjs/operators'
+import { switchMap } from 'rxjs/operators'
 import { ParamMap, ActivatedRoute, Router } from '@angular/router'
-import {
-  SetActiveBrief
-} from '../../../reducers/brief/brief.actions'
+import { SetActiveBrief } from '../../../reducers/brief/brief.actions'
 import { SetActiveBriefPath } from '../../../reducers/navigation/navigation.actions'
 import { EMPTY, BehaviorSubject } from 'rxjs'
 import { FormBuilder } from '@angular/forms'
-import { statuslist } from '../mock-data'
-import { MdcDialog } from '@angular-mdc/web';
+import { statuslist, activityList, notifications, user_notifications } from '../mock-data'
+import { MdcDialog } from '@angular-mdc/web'
 
 @Component({
   selector: 'digital-first-brief-subscription-editor',
@@ -22,49 +18,52 @@ import { MdcDialog } from '@angular-mdc/web';
 })
 export class BriefSubscriptionEditorComponent implements OnInit {
   public background$: BehaviorSubject<string> = new BehaviorSubject('#455a64')
-  documentStatusList$: BehaviorSubject<any>
-  brief$: any
-  selectId$: any
+  public brief$: any
+  public selectId$: any
+  public documentStatusList$: BehaviorSubject<any>
+  public activities$: BehaviorSubject<any>
+  public notifications$: BehaviorSubject<any>
 
-  activities$: BehaviorSubject<any>
-  activeBriefId: string;
+  public activeBriefId: string
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<fromRoot.State>,
-    private fb: FormBuilder, 
+    private fb: FormBuilder,
     public dialog: MdcDialog
   ) {}
 
   ngOnInit() {
     this.brief$ = this.store.pipe(select(fromBrief.selectBriefState))
     this.documentStatusList$ = new BehaviorSubject(statuslist)
-    this.activities$ = new BehaviorSubject([
-      { caption: `Decision` },
-      { caption: `New Comments` },
-      { caption: `New Documents` },
-      { caption: `Updates and Changes` }
-    ])
+    this.activities$ = new BehaviorSubject(activityList)
+    this.notifications$ = new BehaviorSubject([])
 
     this.selectId$ = this.route.paramMap
-    .pipe(
-      switchMap((params: ParamMap) => {
-        this.activeBriefId = params.get('id')
+      .pipe(
+        switchMap((params: ParamMap) => {
+          this.activeBriefId = params.get('id')
 
-        console.log(`🍏`, this.activeBriefId)
+          console.log(`🍏`, this.activeBriefId)
 
-        this.store.dispatch(
-          new SetActiveBrief({ activeBriefId: this.activeBriefId })
-        )
-        this.store.dispatch(
-          new SetActiveBriefPath({ activeBriefId: this.activeBriefId })
-        )
+          this.store.dispatch(
+            new SetActiveBrief({ activeBriefId: this.activeBriefId })
+          )
+          this.store.dispatch(
+            new SetActiveBriefPath({ activeBriefId: this.activeBriefId })
+          )
+  
+          const users = user_notifications.filter(u => `${u.brief_id}` === this.activeBriefId)
+          this.notifications$.next(users)
+          return EMPTY
+        })
+      )
+      .subscribe()
+  }
 
-        return EMPTY
-      })
-    )
-    .subscribe()
+  checked(arr, id) {
+    return id in arr.map(p=> p.id)
   }
 
   handleView($event) {
