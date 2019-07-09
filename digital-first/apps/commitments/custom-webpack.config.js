b31a7702db8f71f9ef75c03a820e39d58fd73ae7
Ning <ningmengvv@gmail.com>
2019-07-09 18:08:53 +1000

    sharepoint deployment

--- digital-first/apps/commitments/custom-webpack.config.js
+++ digital-first/apps/commitments/custom-webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         pageLanguageDirective: `<%@ Page Language="C#" %>`,
         tagPrefixDirective: `<%@ Register tagprefix="SharePoint" namespace="Microsoft.SharePoint.WebControls" assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>`,
         basePath: `<base runat="server" href="<% $SPUrl:~site/SitePages/commitments.aspx/ %>" />`,
-        favIcon: `<link rel="icon" type="image/png" sizes="32x32" href="<% $SPUrl:~site/SiteAssets/apps/css/images/favicon/favicon-32x32.png %>">`
+        favIcon: `<link rel="icon" type="image/png" sizes="32x32" href="<% $SPUrl:~site/SiteAssets/apps/commitments/favicon/favicon-32x32.png %>">`
       },
       chunks: [
         'runtime',
