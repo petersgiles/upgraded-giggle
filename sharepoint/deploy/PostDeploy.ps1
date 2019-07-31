e6b6cd50d183b0d879cfc064e1d01993adc0ef69
Ning <ningmengvv@gmail.com>
2019-07-31 16:06:12 +1000

    merge

--- sharepoint/deploy/PostDeploy.ps1
+++ sharepoint/deploy/PostDeploy.ps1
@@ -5,22 +5,18 @@ param(
     [string]$SiteConfiguration,
     [string]$LoadReferenceData
 )
-$devSiteName = $null 
-if ($OctopusParameters)
-{
+$devSiteName=""
+if($OctopusParameters) {
     $SiteUrls = $OctopusParameters["SiteUrls"]
     $AppName = $OctopusParameters["AppName"]
     $LoadReferenceData = $OctopusParameters["LoadReferenceData"]
     $SiteConfiguration = $OctopusParameters["SiteConfiguration"]
     $ForceSchemaUpdate = $OctopusParameters["ForceSchemaUpdate"]
 }
-else
-{
-    $devSiteName = (Read-Host "Please type your dev site name, e.g. Pete, Kim, Tim, Ning: ").Trim('/')
-    
+else{
+    $devSiteName=Read-Host "Please type your dev site name, e.g. Pete, Kim, Tim, Ning: "
 }
-if ($PSScriptRoot)
-{
+if ($PSScriptRoot) {
     Set-Location $PSScriptRoot
 }
 
@@ -28,36 +24,35 @@ $binPath = (Get-Item "scripts").FullName
 $deploySites = $SiteUrls.Split(',')
 Write-Host "Site Configuration : $SiteConfiguration"
 
-if ($null -eq $ForceSchemaUpdate -or $ForceSchemaUpdate -ne "True")
-{
+if ($null -eq $ForceSchemaUpdate -or $ForceSchemaUpdate -ne "True") {
     $boolForceUpdateSchema = $false
 }
-else
-{
+else {
     $boolForceUpdateSchema = $true
 }
 
-foreach ($deploySiteUrl in $deploySites)
-{
-    if ($devSiteName)
+foreach ($deploySiteUrl in $deploySites) {
+    if($devSiteName -ne "")
     {
-        $deploySiteUrl = $deploySiteUrl + "/" + $devSiteName
+        if($devSiteName[-1] -eq '/'){
+            $deploySiteUrl=$deploySiteUrl+$devSiteName
+        }
+        else{
+            $deploySiteUrl=$deploySiteUrl+"/"+$devSiteName
+        }
     }
-    
     Write-Host "Deploying to URL $deploySiteUrl"
-    & .\scripts\BulkUploadSharePointCSOM.ps1 -Folder "SiteAssets" -DocLibName "Site Assets" -binPath $binPath -SiteUrl $deploySiteUrl -jsOnly:$jsOnly.IsPresent
-    & .\scripts\BulkUploadSharePointCSOM.ps1 -Folder "SitePages" -DocLibName "Site Pages" -binPath $binPath -SiteUrl $deploySiteUrl -jsOnly:$jsOnly.IsPresent
+    & .\scripts\BulkUploadSharePointCSOM.ps1 -Folder "SiteAssets/" -DocLibName "Site Assets" -binPath $binPath -SiteUrl $deploySiteUrl -jsOnly:$jsOnly.IsPresent
+    & .\scripts\BulkUploadSharePointCSOM.ps1 -Folder "SitePages/" -DocLibName "Site Pages" -binPath $binPath -SiteUrl $deploySiteUrl -jsOnly:$jsOnly.IsPresent
     & .\scripts\Deploy-Lists.ps1 -saveLocation "ListDefinitions/$AppName" -binPath $binPath -siteUrl $deploySiteUrl -forceListUpdate $boolForceUpdateSchema
     
-    if ($LoadReferenceData -eq 'True')
-    {
+    if ($LoadReferenceData -eq 'True') {
         Write-Host "Loading reference data"
         & .\scripts\ImportAll-ListDataFromXml.ps1 -importLocation "ListData/$AppName" -binPath $binPath -SiteUrl $deploySiteUrl
     }
 }
 
-if ((-not $null -eq $SiteConfiguration))
-{
+if ((-not $null -eq $SiteConfiguration)) {
     & .\scripts\Set-SiteConfiguration.ps1 `
         -binPath $binPath `
         -appName $AppName `
