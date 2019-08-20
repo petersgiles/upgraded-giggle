50cf256739bcdab80b419be302b8a3c66bdb1f5a
Peter Giles <peter.s.giles@gmail.com>
2019-08-20 11:34:55 +1000

    outcomes

--- digital-first/apps/policy-briefs/src/app/pages/brief/brief-recommendation-action/brief-recommendation-action.component.ts
+++ digital-first/apps/policy-briefs/src/app/pages/brief/brief-recommendation-action/brief-recommendation-action.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'digital-first-brief-recommendation-action',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brief-recommendation-action.component.scss']
 })
 export class BriefRecommendationActionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() recommendation
+
+  form = new FormGroup({
+    outcome: new FormControl(''),
+  });
 
   ngOnInit() {
+    
   }
-
 }
