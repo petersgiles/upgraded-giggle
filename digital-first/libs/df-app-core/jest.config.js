2c5cf64a40c6fe68ff228b0bdf67efed55001edc
Ning <ningmengvv@gmail.com>
2019-09-05 14:16:23 +1000

    continue updates

--- digital-first/libs/df-app-core/jest.config.js
+++ digital-first/libs/df-app-core/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   name: 'df-app-core',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/df-app-core'
-}
+  coverageDirectory: '../../coverage/libs/df-app-core',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
