89ee080e6e0828ae88999cffb13a48f1748907c5
Ning <ningmengvv@gmail.com>
2019-09-05 12:12:23 +1000

    copy files from new nx workspace

--- digital-first/jest.config.js
+++ digital-first/jest.config.js
@@ -1,24 +1,10 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: ["<rootDir>/node_modules/jest-preset-angular/InlineHtmlStripStylesTransformer"],
-    },
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!@ngrx|angular2-ui-switch|ng-dynamic|@material|bryntum-scheduler|ng2-charts)'],
-    testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-    transform: {
-      '^.+\\.(ts|js|html)$': 'ts-jest',
-     // '^.+\\.tsx?$': 'babel-jest',
-    // '^.+\\.(ts|html)$': 'ts-jest',
-    // '^.+\\.js?$': 'babel-jest',
-    },
-     moduleNameMapper: {
-      '^bryntum-scheduler/(.*)$': '<rootDir>/apps/commitments-reader/src/lib/testing/DataHelper.ts'
-    }, 
-    resolver: '@nrwl/jest/plugins/resolver',
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    collectCoverage: true,
-    coverageReporters: ['html']
+  resolver: '@nrwl/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html'],
+  passWithNoTests: true
    }
