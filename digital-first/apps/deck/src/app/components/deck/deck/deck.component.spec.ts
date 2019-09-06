a0437f8c948c415944f71e52ca08141d85565102
Ning <ningmengvv@gmail.com>
2019-09-06 10:53:23 +1000

    half done test

--- digital-first/apps/deck/src/app/components/deck/deck/deck.component.spec.ts
+++ digital-first/apps/deck/src/app/components/deck/deck/deck.component.spec.ts
@@ -1,46 +1,125 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-import { FormBuilder } from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing'
+import { Router } from '@angular/router'
+import { By } from '@angular/platform-browser'
+import { DeckComponent } from './deck.component'
+import { NgSelectModule } from '@ng-select/ng-select'
+import { NgxWigModule } from 'ngx-wig'
 import {
-  Overlay,
   MdcListModule,
   MdcRippleModule,
+  MdcTextFieldModule,
   MdcCardModule
 } from '@angular-mdc/web'
-import { Store } from '@ngrx/store'
-import { DfPipesModule, } from '@digital-first/df-pipes';
-import { SafeHtmlPipe } from '@df/components/lib/pipes/safe-html.pipe';
-import { UserProfileComponent } from '../../../pages/user-profile/user-profile.component';
-
-describe('UserProfileComponent', () => {
-  let component: UserProfileComponent
-  let fixture: ComponentFixture<UserProfileComponent>
+import { ReactiveFormsModule } from '@angular/forms'
+import { EditCardComponent } from '../editor/edit-card/edit-card.component'
+import {
+  StandardCardComponent,
+  RefinerCardComponent,
+  ParentCardComponent,
+  MarkdownCardComponent,
+  EmbedCardComponent,
+  ChartCardComponent,
+  AudioCardComponent,
+  ImageCardComponent,
+  VideoCardComponent,
+  TestCardComponent
+} from '../cards'
+import {
+  CardMenuComponent,
+  CardSupportingTextComponent,
+  CardActionsComponent,
+  CardTitleComponent,
+  CardDataComponent
+} from '../parts'
+import { ChartsModule } from 'ng2-charts'
+import { SafeHtmlPipe } from 'libs/df-pipes/src/lib/safe-html.pipe'
+describe('DeckComponent', () => {
+  debugger
+  let component: DeckComponent
+  let fixture: ComponentFixture<DeckComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [DfPipesModule, MdcListModule, MdcRippleModule, MdcCardModule],
-      declarations: [UserProfileComponent],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        MdcTextFieldModule,
+        NgSelectModule,
+        NgxWigModule,
+        MdcListModule,
+        MdcRippleModule,
+        MdcCardModule,
+        ChartsModule
+      ],
       providers: [
         SafeHtmlPipe,
-        FormBuilder,
-        Overlay,
         {
-          provide: Store,
-          useValue: {
-            pipe: jest.fn(),
-            dispatch: jest.fn()
-          }
-        }
+          provide: Router,
+          useValue: { navigate: jest.fn()}
+        },
+      ],
+      declarations: [
+        DeckComponent,
+        EditCardComponent,
+        StandardCardComponent,
+        RefinerCardComponent,
+        ParentCardComponent,
+        MarkdownCardComponent,
+        EmbedCardComponent,
+        ChartCardComponent,
+        AudioCardComponent,
+        ImageCardComponent,
+        VideoCardComponent,
+        TestCardComponent,
+        CardMenuComponent,
+        CardSupportingTextComponent,
+        CardActionsComponent,
+        CardTitleComponent,
+        CardDataComponent
       ]
     }).compileComponents()
   }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserProfileComponent)
+    fixture = TestBed.createComponent(DeckComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should create new card on click', () => {
+    const btn = fixture.debugElement.query(By.css('button'))
+  })
+
+  it('should emit onAction event', done => {
+    let action = {
+      actions: [
+        {
+          title: 'Get Started',
+          url: '/dashboard',
+          cardType: 'Parent',
+          colour: 'IGBGreen',
+          data: false,
+          id: '2',
+          media: null,
+          parent: null,
+          size: '4',
+          sortOrder: '9',
+          supportingText: `<div class='ExternalClassEC844ABB266C4CF7857F776E0B745CCF'><p>test </p></div>`,
+          titleClass: 'IGBGreen'
+        }
+      ]
+    }
+
+    let parentCard = new ParentCardComponent()
+    parentCard.onAction.subscribe(res => {
+      expect(res.actions[0].title).toEqual('Get Started')
+      done()
+    })
+    parentCard.onAction.emit(action)
   })
 })
