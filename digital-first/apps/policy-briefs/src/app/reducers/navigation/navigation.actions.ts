f5c782de628537e70cbe9b13b828662b030c7ba8
Ning <ningmengvv@gmail.com>
2019-09-06 15:25:34 +1000

    fixed lint errors

--- digital-first/apps/policy-briefs/src/app/reducers/navigation/navigation.actions.ts
+++ digital-first/apps/policy-briefs/src/app/reducers/navigation/navigation.actions.ts
@@ -1,13 +1,13 @@
 import { Action } from '@ngrx/store'
-import { NavigatorTreeNode } from '@df/components';
-import { expand } from 'rxjs/operators';
+import { NavigatorTreeNode } from '@df/components'
+import { expand } from 'rxjs/operators'
 
 export enum NavigationActionTypes {
   GetNavigations = '[Navigation] Get Navigations',
   GetNavigationsFailure = '[Navigation] Get Navigations Failure',
   LoadNavigations = '[Navigation] Load Navigations',
   ToggleExpand = '[Navigation] Toggle Expand',
-  SetActiveBriefPath = '[Navigation] SetActiveBriefPath',
+  SetActiveBriefPath = '[Navigation] SetActiveBriefPath'
 }
 
 export class SetActiveBriefPath implements Action {
@@ -17,27 +17,30 @@ export class SetActiveBriefPath implements Action {
 
 export class GetNavigations implements Action {
   readonly type = NavigationActionTypes.GetNavigations
-
 }
 
 export class ToggleExpand implements Action {
   readonly type = NavigationActionTypes.ToggleExpand
 
-  constructor(public payload: { id: string, expanded: boolean}) {
-  }
+  constructor(public payload: { id: string; expanded: boolean }) {}
 }
 
 export class GetNavigationsFailure implements Action {
   readonly type = NavigationActionTypes.GetNavigationsFailure
 
-  constructor(public payload: any) {
-  }
+  constructor(public payload: any) {}
 }
 
 export class LoadNavigations implements Action {
   readonly type = NavigationActionTypes.LoadNavigations
-  constructor(public payload: { nodes: NavigatorTreeNode[], loading: boolean}) {
-  }
+  constructor(
+    public payload: { nodes: NavigatorTreeNode[]; loading: boolean }
+  ) {}
 }
 
-export type NavigationActions = LoadNavigations | GetNavigations | GetNavigationsFailure | ToggleExpand | SetActiveBriefPath
+export type NavigationActions =
+  | LoadNavigations
+  | GetNavigations
+  | GetNavigationsFailure
+  | ToggleExpand
+  | SetActiveBriefPath
