203b1046b87e3e9ae7f9668b352cf44164e11ed7
Peter Giles <peter.s.giles@gmail.com>
2019-09-09 11:13:53 +1000

    commitments

--- digital-first/apps/policy-briefs/src/app/pages/brief/brief-commitments/brief-commitments.component.ts
+++ digital-first/apps/policy-briefs/src/app/pages/brief/brief-commitments/brief-commitments.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input } from '@angular/core'
 import * as fromRoot from '../../../reducers/index'
 import * as fromBrief from '../../../reducers/brief/brief.reducer'
 import { Store, select } from '@ngrx/store'
+import { DeleteBriefCommitmentGQL, SaveBriefCommitmentGQL, CommitmentsLbsGQL } from '../../../generated/graphql';
+import { EMPTY } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ParamMap, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'digital-first-brief-commitments',
@@ -10,10 +14,34 @@ import { Store, select } from '@ngrx/store'
 })
 export class BriefCommitmentsComponent implements OnInit {
   brief$: any
-  constructor(private store: Store<fromRoot.State>) {}
+  canRemove: boolean
+  briefCommitments = null
+  briefCommitmentsLookup = []
+  selectId$: any;
+  activeBriefId: string;
+
+  constructor( 
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<fromRoot.State>, 
+    // private commitmentsLBS: CommitmentsLbsGQL,
+    // private deleteBriefCommitmentGQL: DeleteBriefCommitmentGQL,
+    // private saveBriefCommitmentGQL: SaveBriefCommitmentGQL
+    ) {}
 
   ngOnInit() {
     this.brief$ = this.store.pipe(select(fromBrief.selectBriefState))
+
+    this.selectId$ = this.route.paramMap
+    .pipe(
+      switchMap((params: ParamMap) => {
+        this.activeBriefId = params.get('id')
+
+        return EMPTY
+      })
+    )
+    .subscribe()
+
   }
 
   navigateToCommitment(commitment) {}
